e78305a34d99bd52d41f69814b128d55f71c7740
fernandogbz <fgjv06@gmail.com>
2024-10-26 18:45:46 -0300

    loader function added

--- client/script.js
+++ client/script.js
@@ -3,3 +3,17 @@ import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat-container");
+
+loadInterval;
+
+function loader(e) {
+  e.textContent = "";
+
+  loadInterval = setInterval(() => {
+    e.textContent += ".";
+
+    if (e.textContent.length > 3) {
+      e.textContent = "";
+    }
+  }, 300);
+}
